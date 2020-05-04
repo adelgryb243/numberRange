d99ce7e9b63bb0e336777b7ef0f6ed1799a49f02
Adele Gryba <adelgryb243@ed.amdsb.ca>
2020-05-04 19:31:39 -0400

    Add project files.

---
+++ numberRange/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace numberRange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.endBox = new System.Windows.Forms.TextBox();
+            this.startBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.goButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // endBox
+            // 
+            this.endBox.Location = new System.Drawing.Point(316, 206);
+            this.endBox.Name = "endBox";
+            this.endBox.Size = new System.Drawing.Size(39, 20);
+            this.endBox.TabIndex = 0;
+            // 
+            // startBox
+            // 
+            this.startBox.Location = new System.Drawing.Point(316, 165);
+            this.startBox.Name = "startBox";
+            this.startBox.Size = new System.Drawing.Size(39, 20);
+            this.startBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(161, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter a starting number!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter an ending number!";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Ink Free", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(244, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(273, 46);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Its Loop Time!";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(64, 259);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(692, 165);
+            this.outputLabel.TabIndex = 5;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // goButton
+            // 
+            this.goButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.goButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
+            this.goButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.goButton.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goButton.Location = new System.Drawing.Point(429, 165);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(188, 71);
+            this.goButton.TabIndex = 6;
+            this.goButton.Text = "show me the values!";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startBox);
+            this.Controls.Add(this.endBox);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox endBox;
+        private System.Windows.Forms.TextBox startBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button goButton;
+    }
+}
+
---
+++ numberRange/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numberRange
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            int start = Convert.ToInt32(startBox.Text);
+            int end = Convert.ToInt32(endBox.Text);
+          
+            for (int y = start; y <= end; y++)
+            {
+                outputLabel.Text += y + " ";
+                outputLabel.Update();
+                System.Threading.Thread.Sleep(100);
+            }
+  
+        }
+    }
+}
